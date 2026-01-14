f5f90b38e4d9f1f85ad67e3d2176709ab5aec792
Harahan <yh4717023@gmail.com>
2026-01-15 05:37:22 +0800

    Add HPSv3 as a submodule

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -8,8 +8,6 @@ export HF_DATASETS_CACHE=/data/.cache/huggingface/datasets
 export XDG_CACHE_HOME=/data/.cache
 export TOKENIZERS_PARALLELISM=false
 export NCCL_TIMEOUT=18000
-# Reduce CUDA memory fragmentation
-export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 CONFIG_PATH=config/longcat.yaml
 
 accelerate launch \
